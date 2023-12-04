63c83627277995100f1317e5ce63e97d4b35c62c
Oleh <80891624+ZeDarkAdam@users.noreply.github.com>
2023-12-04 12:21:28 +0200

    Update Github_Assets_Downloader.py

--- Github_Assets_Downloader.py
+++ Github_Assets_Downloader.py
@@ -31,78 +31,104 @@ def bytes_to_megabytes(bytes_size):
     return rounded_megabytes
 
 
+
 def get_github_repo_assets_container(owner, repo_name, extension_filter):
 
-    with st.spinner(f'Getting the latest version of {repo_name}...'):
-        time.sleep(0.5)
+    with st.container(border=True):
 
+        with st.spinner(f'Getting the latest version of {repo_name}...'):
+            #time.sleep(0.5)
 
-        repo_url = f"https://api.github.com/repos/{owner}/{repo_name}/releases/latest"
-        
-        response = requests.get(repo_url, headers={"Accept": "application/vnd.github+json"})
+            repo_url = f"https://api.github.com/repos/{owner}/{repo_name}/releases/latest"
 
-        if response.status_code == 200:
-            ###########################################################################
-            release_info = response.json()
-            #print("From repo:", repo_url)
-            #st.write("Release date:", release_info["published_at"])
+            response = requests.get(repo_url, headers={"Accept": "application/vnd.github+json"})
 
-            ###########################################################################
-            release_date= release_info["published_at"]
-            release_date_parsed = datetime.strptime(release_date, '%Y-%m-%dT%H:%M:%SZ')
-            time_difference = datetime.utcnow() - release_date_parsed
+            if response.status_code == 200:
+                ###########################################################################
+                release_info = response.json()
+                #print("From repo:", repo_url)
+                #st.write("Release date:", release_info["published_at"])
 
-            hours = time_difference.total_seconds() // 3600
-            days = time_difference.days
-            weeks = math.ceil(time_difference.days / 7)
-            months = time_difference.days // 30.44
-            
-            if months > 0:
-                ago = "months ago"
-                time_num = months
-            elif weeks > 0:
-                ago = "weeks ago"
-                time_num = weeks
-            elif days > 0:
-                ago = "days ago"
-                time_num = days
-            elif hours >= 0 : 
-                ago = "hours ago"
-                time_num = hours
-            ###########################################################################
-            
-            with st.container(border=True):
+                ###########################################################################
+                release_date= release_info["published_at"]
+                release_date_parsed = datetime.strptime(release_date, '%Y-%m-%dT%H:%M:%SZ')
+                time_difference = datetime.utcnow() - release_date_parsed
+
+                hours = time_difference.total_seconds() // 3600
+                days = time_difference.days
+                weeks = math.ceil(time_difference.days / 7)
+                months = time_difference.days // 30.44
                 
+                if months > 0:
+                    ago = "months ago"
+                    time_num = months
+                elif weeks > 0:
+                    ago = "weeks ago"
+                    time_num = weeks
+                elif days > 0:
+                    ago = "days ago"
+                    time_num = days
+                elif hours >= 0 : 
+                    ago = "hours ago"
+                    time_num = hours
+                ###########################################################################
+                
+                #with st.container(border=True):
+                    
                 st.subheader(f"**{repo_name}**\nUpdated **{int(time_num)}** {ago}", divider='grey')
 
-                #st.subheader(f"**{repo_name}**") # , divider='red'
                 #st.write(f"Updated **{days}** {ago}")
                 # st.divider()
 
                 #st.subheader("", divider='gray')
 
-                assets = release_info["assets"] 
-                if assets:
 
-                    #num = 0
-                    for asset in assets:
-                        if extension_filter in asset["name"]:
-                            
-                            # if num != 0 : st.write(" ")
-                            # num += 1
-                            st.write(" ")
-                            mb_size = bytes_to_megabytes(asset["size"])
-                            st.success(f'[**{asset["name"]}**]({asset["browser_download_url"]}) ({mb_size} MB)')
-                            st.caption(f'Downloaded: **{asset["download_count"]}** times')
+                tab1, tab2 = st.tabs(["Download", "Changelog"])
+
+                
+                with tab1:
+                    all_ext = st.toggle(f'Show all file extensions', value = False, key = repo_name)
+                    if all_ext == False:   
+                        extension_filter = f".{extension_filter}"
+                    else:
+                        extension_filter = ""
+
+                    assets = release_info["assets"] 
+                    if assets:
+                        for asset in assets:
+                            if extension_filter in asset["name"]:
+                                
+                                st.write(" ")
+                                mb_size = bytes_to_megabytes(asset["size"])
+                                st.success(f'[**{asset["name"]}**]({asset["browser_download_url"]}) ({mb_size} MB)')
+                                #st.link_button(f'Download ({mb_size} MB)', "https://streamlit.io/gallery", use_container_width=True)
+                                st.caption(f'Downloaded: **{asset["download_count"]}** times')
+                                #st.link_button(f'{asset["download_count"]}', asset["browser_download_url"])
+                                
 
 
-                else:
-                    st.error("No assets found.")
+                    else:
+                        st.error("No assets found.")
 
 
-        else:
-            st.error(f"Failed to fetch release information of **{repo_name}**. Status code: [**{response.status_code}**]({repo_url})")
-            #st.toast('**Перевищено ліміт запитів з цього іп, зайдіть пізніше**', icon='🚫')
+
+                with tab2:
+                    st.header("Тут поки нічого немає")
+
+
+
+
+
+
+
+
+
+
+
+
+            else:
+                st.error(f"Failed to fetch release information of **{repo_name}**. Status code: [**{response.status_code}**]({repo_url})")
+                #st.toast('**Перевищено ліміт запитів з цього іп, зайдіть пізніше**', icon='🚫')
 
 
 
@@ -110,13 +136,13 @@ def get_github_repo_assets_container(owner, repo_name, extension_filter):
 
 st.title("Github Assets Downloader")
 
-owner, repo_name, extension_filter = "NoName-exe", "revanced-extended", ".apk"
+owner, repo_name, extension_filter = "NoName-exe", "revanced-extended", "apk"
 get_github_repo_assets_container(owner, repo_name, extension_filter)
 
 
-#st.write("")
-#owner, repo_name, extension_filter = "Team-xManager", "xManager", ".apk"
-#get_github_repo_assets_container(owner, repo_name, extension_filter)
+# st.write("")
+# owner, repo_name, extension_filter = "Team-xManager", "xManager", ".apk"
+# get_github_repo_assets_container(owner, repo_name, extension_filter)
 
 
 # st.write("")
